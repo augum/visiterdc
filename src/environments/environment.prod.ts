914ddd850853bf2f52624cda01bc9e674fdb72e2
augum <augumakuma@gmail.com>
2020-06-17 13:48:29 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDST9Z5pDYmGRgOLgb54watkaCK16T8bUA",
+    authDomain: "visiterdc-f4133.firebaseapp.com",
+    databaseURL: "https://visiterdc-f4133.firebaseio.com",
+    projectId: "visiterdc-f4133",
+    storageBucket: "visiterdc-f4133.appspot.com",
+    messagingSenderId: "51790969854",
+    appId: "1:51790969854:web:b796fd17af213788d41eaf",
+    measurementId: "G-D5MYGXBPX9"
+  }
 };
